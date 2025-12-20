c3a866eb1f0b3d4f724d8a6c918daeff529925c3
Querp <dhofland@gmail.com>
2025-12-20 03:00:58 +0100

    sdf

--- articles/settings.py
+++ articles/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +129,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = []
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Optional: compress and cache static files
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
