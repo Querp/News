c8034b3227478ef673f3269a3cde04623ab6eb7c
Querp <dhofland@gmail.com>
2025-12-25 11:47:16 +0100

    sdfef3  3f

--- articles/settings.py
+++ articles/settings.py
@@ -10,16 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
 import dj_database_url
-# from dotenv import load_dotenv
+from pathlib import Path
+from dotenv import load_dotenv
 
-# Load Env variables locally 
-# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load Env variables locally 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
